c16b32b2e68ca2092531411b6f48cdd884e4f56c
Vitaliy Hnatyk <vitaliy.hnatyk@gnail.com>
2024-10-10 17:05:22 +0300

    refactored code

--- src/pagination.js
+++ src/pagination.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useTranslation, initReactI18next } from "react-i18next";
+import { useTranslation, I18nextProvider } from "react-i18next";
 import i18next from './i18n';
 
 
